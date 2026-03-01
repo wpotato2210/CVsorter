01cf4bd17fb56664efe11f0ca67d427f62469c66
wpotato2210 <w.thian@hotmail.com>
2026-03-01 14:19:33 +1300

    Add files via upload
    Openspec files

---
+++ tests/test_preprocess.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestPreprocess(unittest.TestCase):
+    def test_extract_lanes(self):
+        self.assertTrue(True)
---
+++ tests/test_serial_interface.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestSerialInterface(unittest.TestCase):
+    def test_send_command(self):
+        self.assertTrue(True)
